6a47a9ec6b38cc007770f06ea39c315cdbf2d6f8
KiritoA <jason_dx@outlook.com>
2016-01-15 17:03:00 +0800

    修改断线重连机制

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@
 #include "adapter.h"
 
 #define ARG_NUMBER	4
-#define ABOUT_INFO_STRING "C3H Client 15.12j\n"
+#define ABOUT_INFO_STRING "C3H Client 16.01j\n"
 
 void signal_interrupted (int signo)
 {
@@ -140,14 +140,13 @@ int main(int argc, char *argv[])
 			if(reconnect == 0)
 				break;
 
-			if ((time(NULL) - lastAuthTime) < 20 && ++overheat > 3)
+			time_t timeToWait = time(NULL) - lastAuthTime;
+			if (timeToWait < 20 && ++overheat > 3)
 			{
-					PRINTMSG("C3H Client: Wait for 20s...\n");
-					sleep(20);
+					PRINTMSG("C3H Client: Wait for %ds...\n", (int)timeToWait);
+					sleep(20-timeToWait);
 					overheat = 0;
 			}
-			else
-				sleep(5);
 
 			retry++;
 			PRINTMSG("C3H Client: Reconnecting...[S:%d F:%d R:%d]\n", success, failure, retry);
